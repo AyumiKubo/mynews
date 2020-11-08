ad7db810c693fcad37fbe9be96739fb0fb12f3ce
AyumiKubo <ec2-user@ip-172-31-21-162.ap-northeast-1.compute.internal>
2020-11-08 08:03:29 +0000

    task_14

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -19,6 +19,11 @@ class ProfileController extends Controller
     {
         $this->validate($request, Profiles::$rules);
         
+        $profiles = new Profiles;
+        $form = $request->all();
+        
+        unset($form['_token']);
+        
         return redirect('admin/profile/create');
     }
     
